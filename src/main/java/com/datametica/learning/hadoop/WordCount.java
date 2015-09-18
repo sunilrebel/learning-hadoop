a4bf991df55025193b604b022bd4d1cfe1118805
sunil kumar <sunil.kumar@datametica.com>
2015-09-18 18:28:36 +0530

    Added Hello World Program

--- src/main/java/com/datametica/learning/hadoop/WordCount.java
+++ src/main/java/com/datametica/learning/hadoop/WordCount.java
@@ -1,11 +1,41 @@
 package com.datametica.learning.hadoop;
 
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-public class WordCount extends Configured implements Tool {
+public class WordCount {
 
-    public int run(String[] strings) throws Exception {
-        return 0;
+    public static void main(String[] args) throws Exception{
+        if (args.length < 2) {
+            System.out.println("Please give input and output directories properly");
+            System.exit(-1);
+        }
+        Job job = Job.getInstance(new Configuration());
+
+        job.setJobName("WordCountJob");
+        job.setJarByClass(WordCount.class);
+
+        /**
+         * It is setting input paths as well as telling JobTracker that
+         * how many number of input split files are there for this input file
+         */
+        FileInputFormat.setInputPaths(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+
+        job.setMapperClass(WordMapper.class);
+        job.setReducerClass(WordReducer.class);
+
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        job.waitForCompletion(true);
     }
 }
---
+++ src/main/java/com/datametica/learning/hadoop/WordMapper.java
@@ -0,0 +1,21 @@
+package com.datametica.learning.hadoop;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String str = value.toString();
+        for (String word : str.split(" ")) {
+            if (word.length() > 0) {
+                context.write(new Text(word), new IntWritable(1));
+            }
+        }
+    }
+}
