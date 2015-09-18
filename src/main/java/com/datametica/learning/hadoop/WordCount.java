481c9fd20a939df9a6bc69a87c1bcbe0a7ac38cf
sunil kumar <sunil.kumar@datametica.com>
2015-09-18 16:42:37 +0530

    initial version

---
+++ src/main/java/com/datametica/learning/hadoop/WordCount.java
@@ -0,0 +1,11 @@
+package com.datametica.learning.hadoop;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class WordCount extends Configured implements Tool {
+
+    public int run(String[] strings) throws Exception {
+        return 0;
+    }
+}
