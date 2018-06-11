a2f77ea88dcfdfe0b0db9e40f482bf8cabfcc0f8
Sunil <sunil@spann.io>
2018-06-11 12:01:42 +0530

    [Sunil] Changed project name and package

--- src/main/java/io/spann/learning/hadoop/WordCount.java
+++ src/main/java/io/spann/learning/hadoop/WordCount.java
@@ -1,4 +1,4 @@
-package com.datametica.learning.hadoop;
+package io.spann.learning.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
--- src/main/java/io/spann/learning/hadoop/WordMapper.java
+++ src/main/java/io/spann/learning/hadoop/WordMapper.java
@@ -1,4 +1,4 @@
-package com.datametica.learning.hadoop;
+package io.spann.learning.hadoop;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
