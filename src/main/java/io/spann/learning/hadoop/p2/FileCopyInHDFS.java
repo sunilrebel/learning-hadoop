15cebcd75c6f2d4eec98675a25f5d7dd3e7b6e69
Sunil <sunil@spann.io>
2018-06-12 13:58:44 +0530

    [Sunil] Added basic file reading from hadoop via Java program

---
+++ src/main/java/io/spann/learning/hadoop/p2/FileCopyInHDFS.java
@@ -0,0 +1,27 @@
+package io.spann.learning.hadoop.p2;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+public class FileCopyInHDFS {
+
+    public static void main(String[] args) throws IOException {
+        String uri = "hdfs://quickstart.cloudera:8020/sunil/myfile.txt";
+        Configuration conf = new Configuration();
+//        conf.set("yarn.resourcemanager.address", "quickstart.cloudera:8050"); // see step 3
+//        conf.set("mapreduce.framework.name", "yarn");
+//        conf.set("fs.defaultFS", "quickstart.cloudera:8020");
+
+        FileSystem fs = FileSystem.get(URI.create(uri), conf);
+
+        InputStream in = fs.open(new Path(uri));
+        IOUtils.copyBytes(in, System.out, 4096, false);
+        IOUtils.closeStream(in);
+    }
+}
