b213ce4699d3723ab80b5d50adbdcc052a018899
Sunil <sunil@spann.io>
2018-06-12 18:30:04 +0530

    [Sunil] Added partially working DataCoherenceTesting examples

---
+++ src/main/java/io/spann/learning/hadoop/p3/DataCoherenceTesting1.java
@@ -0,0 +1,19 @@
+package io.spann.learning.hadoop.p3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class DataCoherenceTesting1 {
+    public static void main(String[] args) throws IOException {
+        String uri = "hdfs://quickstart.cloudera/sunil";
+        FileSystem fs = FileSystem.get(URI.create(uri), new Configuration());
+
+        Path p = new Path("hdfs://quickstart.cloudera/sunil/newfile");
+        fs.create(p);
+        assert (fs.exists(p)) : "Failed assertion";
+    }
+}
---
+++ src/main/java/io/spann/learning/hadoop/p3/DataCoherenceTesting2.java
@@ -0,0 +1,25 @@
+package io.spann.learning.hadoop.p3;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+public class DataCoherenceTesting2 {
+    public static void main(String[] args) throws IOException {
+        String uri = "hdfs://quickstart.cloudera/sunil";
+        FileSystem fs = FileSystem.get(URI.create(uri), new Configuration());
+
+        Path p = new Path("hdfs://quickstart.cloudera/sunil/newfile");
+        FSDataOutputStream out = fs.create(p);
+        out.write("content".getBytes("UTF-8"));
+        out.flush();
+        out.sync();
+
+        assert (fs.getFileStatus(p).getLen() == 0L) : "Failed assertion";
+    }
+}
